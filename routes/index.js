bae1d9b7436112da2f02269cb6f0bf37b2ef3e59
Dhinesh Nakkeerar <dhinesh@Dhineshs-MacBook-Pro.local>
2015-03-18 22:01:47 -0400

    basic portfolio ready

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Yo Brad!' });
+  res.render('index', { 
+  	title: 'dhineshns',
+  	subtitle : 'Jack Of All Trades, Master Of Captcha'
+  });
 });
 
 module.exports = router;
